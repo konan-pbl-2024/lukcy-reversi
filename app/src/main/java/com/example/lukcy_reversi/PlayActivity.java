302ea76f522ba1aa2a83240fc00c5ad3b89a703d
yuki02112018 <yuki.yamamoto.l.3517@gmail.com>
2024-10-18 17:44:53 +0900

    play画面制作の途中です

--- app/src/main/java/com/example/lukcy_reversi/PlayActivity.java
+++ app/src/main/java/com/example/lukcy_reversi/PlayActivity.java
@@ -1,14 +1,34 @@
 package com.example.lukcy_reversi;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class PlayActivity extends AppCompatActivity {
+
+    private static final int SIZE = 8;
+    private static final int EMPTY = 0;
+    private static final int BLACK = 1;
+    private static final int WHITE = 2;
+    private int[][] board = new int[SIZE][SIZE];
+    private boolean blackTurn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                    board[i][j] = EMPTY;
+            }
+        }
+        board[3][3] = WHITE;
+        board[3][4] = BLACK;
+        board[4][3] = BLACK;
+        board[4][4] = WHITE;
+        }
     }
 }
